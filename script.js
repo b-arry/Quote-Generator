4e6d0953a4a955fd204b732d610919fcae3e981c
aaryabhadgaonkar <aaryabhadgaonkar@gmail.com>
2024-01-28 21:33:02 +0000

    js changes

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test");
+let apiQuotes = [];
+
+# show new quote
+function newQuote() {
+}
+
+# Get quotes from API
+
+async function getQuotes() {
+    cont apiUrl = "https://type.fit/api/quotes/?method=getQuote&lang=en&format=json";
+';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (error){
+    # catch error
+    }
+}
+
+
+# on load
+getQuotes();
